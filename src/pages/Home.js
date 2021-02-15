8ce4bcca7f6dd5554a4fe002dcd2cf41d080cb9b
maizerocom <maizerocom@gmail.com>
2021-02-15 11:50:40 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Home = (props) => {
+  const [history, setHistory] = useState(); 
+  
+  useEffect(() => {
+    const fetchHistory = async () => {
+      const response = await fetch("https://api.spacexdata.com/v3/history");
+      const data = await response.json();
+      setHistory(data);
+    };
+    fetchHistory();
+  }, []);
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>{history.title}</h1>
     </div>
   );
 };
