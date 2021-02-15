2ceeb9c89b86516d07a4a4d183dd687f8448ad6f
Sarun Yantabutr <peekung602@gmail.com>
2021-02-15 20:21:00 +0700

    :bookmark: add nav

---
+++ src/components/Navigation/NavigationItem.js
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const NavigationItem = ({ path, exact, children }) => {
+  return (
+    <li className="border-t md:border-none">
+      <NavLink
+        to={path}
+        className="block md:inline-block px-4 py-3 no-underline text-grey-darkest hover:text-grey-darker"
+        activeClassName="font-bold"
+        exact={exact}
+      >
+        {children}
+      </NavLink>
+    </li>
+  );
+};
+
+export default NavigationItem;
