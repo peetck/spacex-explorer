9283c706ed1f74031bb846ae1ce96f21164bf9c3
Sarun Yantabutr <peekung602@gmail.com>
2021-02-17 23:19:11 +0700

    :white_check_mark: update cursor-pointer on pagination

--- src/components/Launches/PaginationItem.js
+++ src/components/Launches/PaginationItem.js
@@ -1,9 +1,9 @@
 const PaginationItem = (props) => {
   return (
     <div
-      className={`w-12 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in  rounded-full ${
+      className={`w-12 md:flex justify-center items-center hidden leading-5 transition duration-150 ease-in rounded-full ${
         props.active ? "bg-green-700 text-white" : ""
-      }`}
+      } ${props.children !== "..." ? "cursor-pointer" : ""}`}
       onClick={props.onClick}
     >
       {props.children}
