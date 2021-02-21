5460e68c9d3bad3107e8c00b86024d445af00768
Sarun Yantabutr <peekung602@gmail.com>
2021-02-21 18:10:08 +0700

    :adhesive_bandage: image aspect ratio

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom";
 
 import noImage from "../../assets/images/no-image.png";
 
-
 const Card = ({ title, active, imageUrl, date, to, details, isLoading }) => {
   return (
     <div
       className={`flex flex-col cursor-pointer shadow-lg rounded-md w-80 p-3 mx-16 mt-16 transition duration-2000 transform hover:-rotate-3 hover:shadow-2xl`}
     >
       <Link to={to ?? "#"}>
-        <div className="group relative">
-          {isLoading ? (
-            <div className="w-full h-72 rounded-3xl"></div>
-          ) : (
-            <img
-              className={`w-full h-72 rounded-3xl ${details && "p-5"}`}
-              src={imageUrl ?? noImage}
-              alt="Can't load img"
-            />
-          )}
-        </div>
+        {isLoading ? (
+          <div className="w-full h-72 rounded-3xl"></div>
+        ) : (
+          <img
+            className={`object-cover h-72 rounded-3xl ${details && "p-5"}`}
+            src={imageUrl ?? noImage}
+            alt="Can't load img"
+          />
+        )}
 
         <div className="p-5">
           <div className="flex items-center justify-between">
