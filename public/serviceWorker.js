4044fa8991cb8f1210a40e1c7453dd977317e24f
Sarun Yantabutr <peekung602@gmail.com>
2021-02-22 22:07:02 +0700

    :wrench: pwa

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -10,22 +10,20 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match("/offline.html");
-          return cachedResponse;
+  event.respondWith(
+    (async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
         }
-      })()
-    );
-  }
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match("/offline.html");
+        return cachedResponse;
+      }
+    })()
+  );
 });
